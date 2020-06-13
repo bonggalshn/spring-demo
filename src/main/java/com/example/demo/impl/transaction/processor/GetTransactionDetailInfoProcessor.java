da1f9c89d5c5a18422afe29604731a840f450998
Bonggal Siahaan <bonggal.siahaan@gmail.com>
2020-06-14 00:04:17 +0700

    optimize import

--- src/main/java/com/example/demo/impl/transaction/processor/GetTransactionDetailInfoProcessor.java
+++ src/main/java/com/example/demo/impl/transaction/processor/GetTransactionDetailInfoProcessor.java
@@ -1,6 +1,5 @@
 package com.example.demo.impl.transaction.processor;
 
-import com.example.demo.core.model.Money;
 import com.example.demo.core.model.Processor;
 import com.example.demo.impl.transaction.context.TransactionContext;
 import com.example.demo.impl.transaction.result.TransactionDetailResult;
